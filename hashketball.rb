7f45ad3901f95fc216beaeb5d584e659360e6221
Shiro Han <shan99business@gmail.com>
2020-05-27 00:55:07 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -4,12 +4,124 @@ def game_hash
     :home => {
       :team_name => "",
       :colors => [],
-      :players => [{}]
+      :players => [
+        {
+          player_name: "Alan Anderson",
+          number: 0,
+          shoe: 16,
+          points: 22,
+          rebounds: 12,
+          assists: 12,
+          steals: 3,
+          blocks: 1,
+          slam_dunks: 1
+        },
+        {
+          player_name:,
+          number:,
+          shoe:,
+          points:,
+          rebounds:,
+          assists:,
+          steals:,
+          blocks:,
+          slam_dunks:
+        },
+        {
+          player_name:,
+          number:,
+          shoe:,
+          points:,
+          rebounds:,
+          assists:,
+          steals:,
+          blocks:,
+          slam_dunks:
+        },
+        {
+          player_name:,
+          number:,
+          shoe:,
+          points:,
+          rebounds:,
+          assists:,
+          steals:,
+          blocks:,
+          slam_dunks:
+        },
+        {
+          player_name:,
+          number:,
+          shoe:,
+          points:,
+          rebounds:,
+          assists:,
+          steals:,
+          blocks:,
+          slam_dunks:
+        }
+      ]
     },
     :away => {
       :team_name => "",
       :colors => [],
-      :players => [{}]
+      :players => [
+        {
+          player_name:,
+          number:,
+          shoe:,
+          points:,
+          rebounds:,
+          assists:,
+          steals:,
+          blocks:,
+          slam_dunks:
+        },
+        {
+          player_name:,
+          number:,
+          shoe:,
+          points:,
+          rebounds:,
+          assists:,
+          steals:,
+          blocks:,
+          slam_dunks:
+        },
+        {
+          player_name:,
+          number:,
+          shoe:,
+          points:,
+          rebounds:,
+          assists:,
+          steals:,
+          blocks:,
+          slam_dunks:
+        },
+        {
+          player_name:,
+          number:,
+          shoe:,
+          points:,
+          rebounds:,
+          assists:,
+          steals:,
+          blocks:,
+          slam_dunks:
+        },
+        {
+          player_name:,
+          number:,
+          shoe:,
+          points:,
+          rebounds:,
+          assists:,
+          steals:,
+          blocks:,
+          slam_dunks:
+        }
+      ]
     }
   }
 end
